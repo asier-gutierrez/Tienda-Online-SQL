0bafe056e3767e003a004eb468da3a2a1394c1a9
Yasmin <yasmin.martin@opendeusto.es>
2015-12-12 18:33:37 +0100

    Repasar

--- implementacion.sql
+++ implementacion.sql
@@ -116,7 +116,16 @@ ID_CENTRO_OPERACIONES NUMBER(9) CONSTRAINT OF_ID_CENTRO_OPERACIONES_NN NOT NULL 
 
 
 /*EMP*/
-/**/
+/*La tabla empleado esta creada para registrar la informacion de cada empleado que trabaja para la tienda. Esta compuesta por:
+ * Un dni de caracter alfanumerico, no nulo y es a su vez la clave primaria, ya que el empleado sera identificado por esta y no se repite.
+ * El nombre y apellido del usuario en forma alfanumerica y es un campo no nulo, registra el nombre y el apellido de cada empleado.
+ * La fecha de nacimiento de caracter date y no nula.
+ * Un email de caracter varchar y no nulo, ya que se necesita mantenerse en contacto con el empleado.
+ * El NSS de carcter numerico y no nulo, identifica al empleado.
+ * El id del puesto en forma numerica y no nula , provien de la tabla puesto, registra el puesto en el que trabaja el empleado no puede estar vacante.
+ * El id de almacen y oficina de caracter numerico y no nulo, esta hace la funcion de informar en donde trabaja el empleado.
+ * El id de direccion de caracter numerico y no nulo hace referencia al id de la tabla direccion, ya que cada empleado vive en un determinado sitio y se agrupan en direcciones.
+ * El dni del jefe de caracter varchar y no nulo, cada empleado tendra un jefe y este atributo proviene de la misma tabla, ya que el jefe es un empleado.*/
 CREATE TABLE EMPLEADO(
 DNI VARCHAR(9) CONSTRAINT EMP_DNI_NN NOT NULL CONSTRAINT EMP_DNI_PK PRIMARY KEY, 
 NOMBRE VARCHAR(20) CONSTRAINT EMP_NOMBRE_NN NOT NULL,
@@ -132,6 +141,15 @@ DNI_JEFE VARCHAR(9) CONSTRAINT EMP_DNI_JEFE_NN NOT NULL CONSTRAINT EMP_DNI_JEFE_
 );
 
 /*PED*/
+/*La tabla pedido registra la informacion necesaria del pedido realizado. Esta esta compuesta por:
+ * Un id numerico ,no nulo y clave primaria, ya que es el atributo que identifica cada pedido.
+ * Una descripcion de caracter alfanumerico y no nula, esta describe el pedido por ejemplo: si el usuario quiere que tratemos el pedido realizado de alguna forma como para regalo...etc
+ * Una fecha del pedido de caracter date y no nulo, ya que cada pedido se realiza en una fecha en concreto.
+ * El id del estado del pedido de caracter numerico, proviene de la tabla estado_pedido, y se registra el estado en el que se encuentra el pedido.
+ * El numero del dato bancario, es alfanumerico, no nulo y proviene de la tabla dato bancario, con este atributo se registra el dato con el que se ha pagado el pedido.
+ * El dni del empleado de caracter alfanuemerico, no nulo y proveninete de la tabla empleado, es el cual atiende el pedido.
+ * EL dni del usuario de caracter alfanuemerico, no nulo y proveninete de la tabla, es el numero por el cual esta registrado el pedido del usuario en concreto.
+ * La id de direccion numerica, no nula y hace refencia a la misma id de la tabla direccion, ya que cada pedido se deasea que se mande a ciertas direcciones.*/
 CREATE TABLE PEDIDO(
 ID NUMBER(15) CONSTRAINT PED_ID_NN NOT NULL CONSTRAINT PED_ID_PK PRIMARY KEY,
 DESCRIPCION VARCHAR(100),
@@ -144,6 +162,12 @@ ID_DIRECCION NUMBER(9) CONSTRAINT PED_ID_DIRECCION_NN NOT NULL CONSTRAINT PED_ID
 );
 
 /*PRO*/
+/*La tabla producto es creada para registrar la infamcion de los productos que diponemos. Esta consta de:
+ * Un identificador (id) de caracter numerico y no nulo, esta a su vez es declarada como clave primaria, ya que sera el atributo que identifique al prodcuto.
+ * La descripcion del producto en caracter alfanumerico y no nulo, por que es la que contiene las caracteristicas del producto a vender.
+ * Un nombre de caracter alfanumerico y no nulo, para distinguir el produto de los demas cada producto tendra un movil concreto.
+ * El precio de caracter numerico y no nulo, ya que todos los productos tienen un valor de venta.
+ * Y el id del modelo, que proviene de la tabla modelo, es un valor no nulo y esta ON DLETE CASCADE, ya que al ser borrada esa fila tambien se elimira esta.*/
 CREATE TABLE PRODUCTO(
 ID NUMBER (9) CONSTRAINT PRO_ID_NN NOT NULL CONSTRAINT PRO_ID_PK PRIMARY KEY,
 DESCRIPCION VARCHAR(45) CONSTRAINT PRO_DESCRIPCION_NN NOT NULL,
@@ -154,6 +178,14 @@ ID_MODELO NUMBER(9)  CONSTRAINT PRO_ID_MODELO_NN NOT NULL CONSTRAINT PRO_ID_MODE
 
 
 /*RC_QJ*/
+/*La tabla reclamacion_queja esta creada para registrar todas aquellas quejas que tengan los pedidos.
+ * Esta connta de un numero id, que es no es nulo y es la clave primaria, ya que va a ser el atributo identificador de la tabla.
+ * Una denomicion de caracter alfanumerico y no nulo, es el problema en cuestion se necesita la informacion de esta.
+ * Un numero de reclamacion de caracter numerico no nulo y unico, ya que no es posible la repeticion de este.
+ * La descripcion de caracter alfanumerico y no nulo, esta es la informacion del problema detallada
+ * El id del pedido de caracter numerico, proviene de la tabla pedido que esta conectada a esta misma tabla.
+ * El dni del empleado con caracter alfanumerico ya que es el dni, no nulo y hace referencia a la tabla empleado, que es que va a tratar la queja.
+ * El dni del empleado y el id del pedido ambos estan en ON DELETE CASCADE, ya que se quiere eliminar la queja concreta.*/
 CREATE TABLE RECLAMACION_QUEJA( 
 ID NUMBER(9) CONSTRAINT RC_QJ_ID_NN NOT NULL CONSTRAINT RC_QJ_ID_PK PRIMARY KEY,
 DENOMINACION  VARCHAR(20) CONSTRAINT RC_QJ_DENOMINACION_NN NOT NULL,
@@ -164,6 +196,8 @@ DNI_EMPLEADO VARCHAR(9) CONSTRAINT RC_QJ_DNI_EMPLEADO_NN NOT NULL CONSTRAINT RC_
 );
 
 /*DIS*/
+/*La tabla disponibilada esta creada para registrar los modelos de los productos que disponemos en el almacen.
+ * Esta esta compuesta por la id del modelo y la id del almacen, ambas son caracteres numericos no nulos y claves primarias , por que identifica a todas las posibles filas de esas tablas.  */
 CREATE TABLE DISPONIBILIDAD(
 ID_MODELO NUMBER(9) CONSTRAINT DIS_ID_MODELO_NN NOT NULL CONSTRAINT DIS_ID_MODELO_FK_MOD REFERENCES MODELO(ID) ON DELETE CASCADE,
 ID_ALMACEN NUMBER(9) CONSTRAINT DIS_ID_ALMACEN_NN NOT NULL CONSTRAINT DIS_ID_ALMACEN_FK_AL REFERENCES ALMACEN(ID) ON DELETE CASCADE,
@@ -172,6 +206,8 @@ PRIMARY KEY ( ID_MODELO, ID_ALMACEN )
 );
 
 /*DM_EMP*/
+/*La tabla domicilio empleado esta compuesta por el dni del empleado y la id de direccion ambos son claves primarias y no nulas, que para obtener el domicilio del empleado es necesario
+ * saber estos dos campos. Es la tabla que se crea de la union de direccion y empleado.*/
 CREATE TABLE DOMICILIO_EMPLEADO(
 ID_DIRECCION NUMBER(9) CONSTRAINT DM_EMP_ID_DIRECCION_NN NOT NULL CONSTRAINT DM_EMP_ID_DIRECCION_FK_DIR REFERENCES DIRECCION(ID) ON DELETE CASCADE,
 DNI_EMPLEADO VARCHAR(9) CONSTRAINT DM_EMP_DNI_EMPLEADO_NN NOT NULL CONSTRAINT DM_EMP_DNI_EMPLEADO_FK_EMP REFERENCES EMPLEADO(DNI) ON DELETE CASCADE,
@@ -179,6 +215,8 @@ PRIMARY KEY ( ID_DIRECCION, DNI_EMPLEADO )
 );
 
 /*DM_US*/
+/*La tabla domicilio usuario esta compuesta por el dni del usuario y la id de direccion ambos son claves primarias y no nulas, que para obtener el domicilio del usuario es necesario
+ * saber estos dos campos. Es la tabla que se crea de la union de direccion y usuario.*/
 CREATE TABLE DOMICILIO_USUARIO(
 DNI_USUARIO VARCHAR(9) CONSTRAINT DM_US_DNI_USUARIO_NN NOT NULL CONSTRAINT DM_US_DNI_USUARIO_FK_USU REFERENCES USUARIO(DNI) ON DELETE CASCADE,
 ID_DIRECCION NUMBER(9) CONSTRAINT DM_US_ID_DIRECCION_NN NOT NULL CONSTRAINT DM_US_ID_DIRECCION_FK_DIR REFERENCES DIRECCION(ID) ON DELETE CASCADE,
@@ -187,6 +225,9 @@ PRIMARY KEY(DNI_USUARIO,ID_DIRECCION)
 );
 
 /*CNT*/
+/*La tabla contiene es la union entre pedido y producto, en la cual se registra la cantidad de los productos que se han pedido.
+ * La cantidad es un atributo no nulo, ya que si se pide un producto nunca puede ser 0 simpre sera mayor a cero, de hay sacamos la comprobacion (CHECK).
+ * Tambien contiene la id del producto pedido y la id del pedido ambas son claves primarias, ya que se necesitan las dos para hacer las busquedas sobre la cantidad de un pedido o producto concreto.*/
 CREATE TABLE CONTIENE(
 ID_PRODUCTO NUMBER (9) CONSTRAINT CNT_ID_PRODUCTO_NN NOT NULL CONSTRAINT CNT_ID_PRODUCTO_FK_PRO REFERENCES PRODUCTO(ID) ON DELETE CASCADE,
 ID_PEDIDO NUMBER(15) CONSTRAINT CNT_ID_PEDIDO_NN NOT NULL CONSTRAINT CNT_ID_PEDIDO_FK_PED REFERENCES PEDIDO(ID) ON DELETE CASCADE,
